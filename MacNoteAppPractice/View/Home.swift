86c2b66a2fe2d6b7108e11b19661896ee26103c2
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-31 19:35:14 -0600

    --Updating: to min frame width and hieght and resizability

--- MacNoteAppPractice/View/Home.swift
+++ MacNoteAppPractice/View/Home.swift
@@ -8,11 +8,20 @@
 import SwiftUI
 
 struct Home: View {
+    /// List Selection (Going to use this as a Tab to filter the selected category notes)
+    @State private var selectedTag: String?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationSplitView {
+            List(selection: $selectedTag) {
+                
+            }
+        } detail: {
+            
+        }
     }
 }
 
 #Preview {
-    Home()
+    ContentView()
 }
