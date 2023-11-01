4cb92df0686b2e8a7997b6e893549ee9b2f66fd8
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-31 19:27:25 -0600

    --Progress: Setup up the project with Model folder and files and adding a modelContainer to the app

---
+++ MacNoteAppPractice/Model/Note.swift
@@ -0,0 +1,8 @@
+//
+//  Note.swift
+//  MacNoteAppPractice
+//
+//  Created by Mitch Andrade on 10/31/23.
+//
+
+import Foundation
---
+++ MacNoteAppPractice/Model/NoteCategory.swift
@@ -0,0 +1,8 @@
+//
+//  NoteCategory.swift
+//  MacNoteAppPractice
+//
+//  Created by Mitch Andrade on 10/31/23.
+//
+
+import Foundation
