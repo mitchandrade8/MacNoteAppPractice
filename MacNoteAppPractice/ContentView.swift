57e63f8f26a803a15a9e9d586eda1309bb693b69
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-10-31 19:13:15 -0600

    Initial Commit

---
+++ MacNoteAppPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MacNoteAppPractice
+//
+//  Created by Mitch Andrade on 10/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MacNoteAppPractice/MacNoteAppPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacNoteAppPracticeApp.swift
+//  MacNoteAppPractice
+//
+//  Created by Mitch Andrade on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct MacNoteAppPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
